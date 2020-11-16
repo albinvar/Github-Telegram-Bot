151e89510d9211fe6e9bb8a813c9ce4c92dd9bfa
Albin Varghese <albin8285@gmail.com>
2020-11-17 01:26:50 +0530

    get chatId dynamically from user

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ use App\GitHubBot;
 require __DIR__.'/config/config.php';
 
 $api = getApi();
-$chatId = "1145842752";
+$admchatId = "1145842752";
+
+$data = json_decode(file_get_contents("php://input"));
+$chatId = $data->message->chat->id;
 
 $object = new GitHubBot($api, $chatId);
