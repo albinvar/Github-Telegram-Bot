110a40d43185844399b30281111b9da7804cca22
Albin Varghese <albin8285@gmail.com>
2020-12-23 01:53:47 +0530

    set setwebhook.php dynamically

--- setWebhook.php
+++ setWebhook.php
@@ -1,11 +1,12 @@
 <?php
 	require __DIR__.'/config/config.php';
 	
-	$link = $_GET['link'];
-	$botApi = getenv('API_TOKEN')
+	$link= "http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+	//$link = $_GET['link'];
+	$botApi = getenv('API_TOKEN');
 	
-	$text = str_replace("\n", "%0A", $text);
+	
     $method_url = 'https://api.telegram.org/bot' . $botApi . '/setWebhook';
     $url = $method_url . '?url=' . $link;
     $response = @file_get_contents($url);
-    var_dump($response);
+   
