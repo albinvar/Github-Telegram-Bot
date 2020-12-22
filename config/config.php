e443f3dee7cb51e6f2e711514ffa11cbdf229cd8
Albin Varghese <albin8285@gmail.com>
2020-12-22 14:42:05 +0530

    remove unwanted env fields

--- config/config.php
+++ config/config.php
@@ -3,23 +3,19 @@
 require __DIR__.'/../vendor/autoload.php';
 require __DIR__.'/functions.php';
 
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
-$dotenv = new Dotenv();
-$dotenv->loadEnv(__DIR__.'/../.env');
 
 date_default_timezone_set($_ENV['TIMEZONE']);
 
 if (empty(getenv('API_TOKEN')){
-$api = $_ENV['BOT_API'];
+$api = "YOUR_API_TOKEN";
 } else {
 $api = getenv('BOT_TOKEN');
 }
 
 if (empty(getenv('ADMINS')) {
-$admin = $_ENV['ADM_CHATID'];
+$admin = "ADMIN_IDS_SEPERATED_BY_COMMA";
 } else {
 $admin = getenv('ADMINS');
 }
