2660259f0c276149c031d9c152a89586a552ebca
Shamshid <shmshd12@gmail.com>
2020-12-24 10:28:12 +0400

    remove unwanted variable assign and clean codes

--- setWebhook.php
+++ setWebhook.php
@@ -1,11 +1,10 @@
 <?php
-	require __DIR__.'/config/config.php';
-	
-	$link= "https://{$_SERVER['SERVER_NAME']}";
-	$botApi = $api;
-	
-	
-    $method_url = 'https://api.telegram.org/bot' . $botApi . '/setWebhook';
-    $url = $method_url . '?url=' . $link;
-    $response = @file_get_contents($url);
-    var_dump($response);
+
+require __DIR__.'/config/config.php';
+
+$link = "https://{$_SERVER['SERVER_NAME']}";
+$method_url = "https://api.telegram.org/bot{$api}/setWebhook";
+$url = $method_url.'?url='.$link;
+$response = @file_get_contents($url);
+
+var_dump($response);
