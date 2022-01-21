f4c3c856d8096bfc96479f4d5d1b4caab7fc0a2e
Albin Varghese <albin8285@gmail.com>
2022-01-21 17:47:20 +0530

    add functionality to SetWebhook.php

--- src/SetWebhook.php
+++ src/SetWebhook.php
@@ -1,19 +1,25 @@
 <?php
 
+use Longman\TelegramBot\Exception\TelegramException;
+use Longman\TelegramBot\Telegram;
+
 require_once __DIR__.'/../bootstrap/app.php';
 
+//set all configs
 setConfigs();
 
 try {
     // Create Telegram API object
-    $telegram = new Longman\TelegramBot\Telegram(BOT_API_TOKEN, BOT_USERNAME);
+    $telegram = new Telegram(BOT_API_TOKEN, BOT_USERNAME);
 
     // Set webhook
     $result = $telegram->setWebhook(WEBHOOK_URL);
+
+
     if ($result->isOk()) {
         echo $result->getDescription();
     }
-} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+} catch (TelegramException $e) {
     // log telegram errors
     echo $e->getMessage();
 }
