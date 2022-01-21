947bac5959e1813eefe438715850050fd077b585
Albin Varghese <albin8285@gmail.com>
2022-01-21 13:16:31 +0530

    create basic config keys

--- config/bot.php
+++ config/bot.php
@@ -1,1 +1,29 @@
 <?php
+
+/*
+ * Configure your application here.
+ *
+ *
+ *
+ */
+
+return  [
+    /*
+     * Set your bot api token key here.
+     *
+     */
+    'BOT_API_TOKEN' => $_ENV('BOT_API_TOKEN') ?: "YOUR_TELEGRAM_BOT_API_KEY",
+
+    /*
+     * Set your bot username here.
+     */
+    'BOT_USERNAME' => $_ENV('BOT_API_TOKEN') ?: "YOUR_TELEGRAM_BOT_API_KEY",
+
+    /*
+     * ADMINS key expects a single or an array of telegram user ids to push
+     * notifications from a single repositories.
+     *
+     */
+    'ADMINS' => $_ENV('ADMINS') ?: ['ADMIN_1', 'ADMIN_2'],
+
+];
